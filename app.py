9f49f66b2e5ea3393e412fabb75e254cd350a97a
aquaracer <50531938+aquaracer@users.noreply.github.com>
2019-12-16 02:20:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,219 +1,185 @@
+"""
+REST API app.
+"""
 from flask import Flask, jsonify, request
-from models import User, Session, Transactions, Currency, connect
+from models import User, Transactions, connect
 import datetime
-from const import HTTP_BAD_REQUEST, HTTP_NOT_FOUND, HTTP_OK, HTTP_CREATED
+import status_codes as status
+import utils
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'index'
-
-def converter(currency_1, currency_2, sum_1):
-    base = 'USD'
-    with connect() as session:
-        course_currency_1 = session.query(Currency.course).filter(
-                Currency.short_name == currency_1).first()
-        course_currency_1 = course_currency_1[0]
-        multiplicity_currency_1 = session.query(Currency.multiplicity).filter(
-                Currency.short_name == currency_1).first()
-        multiplicity_currency_1 = multiplicity_currency_1[0]
-        course_currency_2 = session.query(Currency.course).filter(
-            Currency.short_name == currency_2).first()
-        course_currency_2 = course_currency_2[0]
-        multiplicity_currency_2 = session.query(Currency.multiplicity).filter(
-            Currency.short_name == currency_2).first()
-        multiplicity_currency_2 = multiplicity_currency_2[0]
-    if currency_1 == base:
-        sum_2 = (course_currency_2 * sum_1)/multiplicity_currency_2
-        return sum_2
-    if currency_2 == base:
-        sum_2 = (multiplicity_currency_2 * sum_1)/course_currency_2
-        return sum_2
-    sum_2 = (multiplicity_currency_1 * sum_1 * course_currency_2) / (multiplicity_currency_2 * course_currency_1)
-    return sum_2
 
 
 @app.route('/registration', methods=['POST'])
 def user_registration():
     result = {}
-    status_code = HTTP_CREATED
-    json = request.get_json() # получаем json из POST запроса
+    status_code = status.HTTP_OK
+    request_data = request.get_json()  # получаем json из POST запроса
     try:
-        login = json['login']
-        password = json['password']
-        account_number = json['account_number']
-        balance = json['balance']
-        currency = json['currency']
-        if len(login) < 7:
-            result['error'] = "Login should contain at least 7 symbols"
+        email = request_data['email']
+        password = request_data['password']
+        account_number = request_data['account_number']
+        balance = request_data['initial_balance']
+        currency = request_data['currency']
+        if len(email) < 6:
+            result['error'] = "Email should contain at least 6 symbols"
             print(result)  # выводим лог в stdout
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
-        if '@' not in login:
-            result['error'] = "Login should contain @"
+        if '@' not in email:
+            result['error'] = "Email should contain @"
             print(result)  # выводим лог в stdout
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
         if len(password) < 3:
             result['error'] = "Password should contain at least 3 symbols"
             print(result)  # выводим лог в stdout
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
-        if len(account_number) != 6:
+        if len(str(account_number)) != 6:
             result['error'] = "Account number should consist of 6 symbols"
             print(result)  # выводим лог в stdout
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
         if balance < 0:
             result['error'] = "Balance can't be negative"
             print(result)  # выводим лог в stdout
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
     except Exception as e:
         print(repr(e))  # выводим лог в stdout
         result['error'] = "JSON does not contain required data"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     with connect() as session:
-        existed_user = session.query(User.login).filter(
-                User.login == login).first()  # проверяем есть ли уже в базе пользователь с заданным логином
-    if existed_user is not None:
+        existed_user = session.query(User.email).filter(
+                User.email == email).first()
+    if existed_user:
         result['error'] = 'This login is busy. Please create another'
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
-    new_user_info = User(login, password, account_number, balance, currency)
+    password = utils.encrypt_password(password)
+    new_user_info = User(email, password, account_number, balance, currency)
     with connect() as session:
         session.add(new_user_info)
-    result['data'] = 'new user has been registered successfully'
+    result['details'] = 'new user has been registered successfully'
     return jsonify(result), status_code
 
 
-@app.route('/auth', methods=['POST'])
-def user_auth():
+@app.route('/login', methods=['POST'])
+def user_login():
     result = {}
-    status_code = HTTP_OK
-    json = request.get_json(force=True)
+    status_code = status.HTTP_OK
+    request_data = request.get_json()  # получаем json из POST запроса
     try:
-        current_login = json['login']
-        current_password = json['password']
+        current_email = request_data['email']
+        current_password = request_data['password']
     except Exception as e:
         result['error'] = "JSON does not contain required data"
         print(repr(e))  # выводим лог в stdout
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     with connect() as session:
-        password = session.query(User.password).filter(User.login == current_login).first() # проверяем есть ли пользователь с заданным логином. если да получаем хеш пароля данного пользователя
-    if password == None:
+        password = session.query(User.password).filter(User.email
+                                                       == current_email).first()
+    if not password:
         result['error'] = "Access denied. This login does not exist"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     password = password[0]
     if current_password != password:
         result['error'] = "Access denied. Invalid password"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
-    result['data'] = 'successful authorization'
+    result['details'] = 'successful authorization'
     return jsonify(result), status_code
 
 
 @app.route('/transaction', methods=['POST'])
 def transaction():
     result = {}
-    status_code = HTTP_OK
-    json = request.get_json()  # получаем json из POST запроса
+    status_code = status.HTTP_OK
+    request_data = request.get_json()  # получаем json из POST запроса
     try:
-        senders_account = json['senders_account']
-        receivers_account = json['receivers_account']
-        amount = json['amount']
+        senders_account = request_data['senders_account']
+        receivers_account = request_data['receivers_account']
+        amount = request_data['amount']
         amount_to_receive = amount
     except Exception as e:
         print(repr(e))  # выводим лог в stdout
         result['error'] = "JSON does not contain required data"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     if amount < 0:
         result['error'] = "You can't send negative amount"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     with connect() as session:
-        senders_account_status = session.query(User.login).filter(
-            User.account_number == senders_account).first()  # проверяем есть ли отправитель
-        receivers_account_status = session.query(User.login).filter(
-            User.account_number == receivers_account).first()  # проверяем есть ли получатель
+        sender = session.query(User.email).filter(
+            User.account_number == senders_account).first()
+        receiver = session.query(User.email).filter(
+            User.account_number == receivers_account).first()
         senders_balance = session.query(User.balance).filter(
-            User.account_number == senders_account).first() # проверяем баланс отправителя
+            User.account_number == senders_account).first()
         receivers_balance = session.query(User.balance).filter(
-            User.account_number == receivers_account).first() # берем баланс получателя
+            User.account_number == receivers_account).first()
         senders_currency = session.query(User.currency).filter(
-            User.account_number == senders_account).first() # берем валюту отправителя
+            User.account_number == senders_account).first()
         receivers_currency = session.query(User.currency).filter(
-            User.account_number == receivers_account).first() # берем валюту получателя
-    if senders_account_status == None:
+            User.account_number == receivers_account).first()
+    if not sender:
         result['error'] = "Account of sender does not exist"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
-    if receivers_account_status == None:
+    if not receiver:
         result['error'] = "Account of receiver does not exist"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
     senders_balance = senders_balance[0]
-    senders_balance -= amount
-    if senders_balance < 0:
+    if (senders_balance - amount) < 0:
         result['error'] = "Not enough funds"
-        status_code = HTTP_BAD_REQUEST
+        status_code = status.HTTP_BAD_REQUEST
         return jsonify(result), status_code
+    senders_balance -= amount
     if senders_currency != receivers_currency:
-        amount_to_receive = converter(senders_currency, receivers_currency, amount)
+        amount_to_receive = utils.converter(senders_currency,
+                                            receivers_currency, amount)
     receivers_balance = receivers_balance[0]
     receivers_balance += amount_to_receive
-    date_time = datetime.datetime.today()
-    date_time = date_time.strftime("%Y-%m-%d-%H.%M.%S")
-    new_transaction = Transactions(date_time, senders_account, receivers_account, amount, senders_currency)
+    timestamp = datetime.datetime.now()
+    timestamp = timestamp.strftime("%Y-%m-%d-%H.%M.%S")
+    new_transaction = Transactions(timestamp, senders_account,
+                                   receivers_account, amount, senders_currency)
     with connect() as session:
-        session.add(new_transaction) # добавляем новую запись в таблицу транзакций
-        session.query(User).filter(User.account_number == senders_account).update({User.balance: senders_balance}) # обновляем баланс отправителя
-        session.query(User).filter(User.account_number == receivers_account).update({User.balance: receivers_balance}) # обновляем баланс получателя
-    result['data'] = 'success'
+        session.add(new_transaction)
+        session.query(User).filter(User.account_number
+                                   == senders_account).update({User.balance:
+                                                                           senders_balance})
+        session.query(User).filter(User.account_number
+                                   == receivers_account).update({User.balance: receivers_balance})
+    result['details'] = 'success'
     return jsonify(result), status_code
 
 
 @app.route('/statement/<account_number>', methods=['GET'])
 def get_statement(account_number):
     result = {}
-    status_code = HTTP_OK
+    status_code = status.HTTP_OK
     with connect() as session:
-        account_status = session.query(User.login).filter(
+        account_status = session.query(User.email).filter(
             User.account_number == account_number).first()
-        if account_status == None:
+        if not account_status:
             result['error'] = "Account does not exist"
-            status_code = HTTP_BAD_REQUEST
+            status_code = status.HTTP_BAD_REQUEST
             return jsonify(result), status_code
         income_transactions = session.query(Transactions).filter(
             Transactions.receivers_account == account_number)
         outcome_transactions = session.query(Transactions).filter(
             Transactions.senders_account == account_number)
-    income = {}
-    outcome = {}
-    for item in income_transactions:
-        cur_dict = {}
-        cur_dict['senders_account'] = item.senders_account
-        cur_dict['receivers_account'] = item.receivers_account
-        cur_dict['amount'] = item.amount
-        cur_dict['currency'] = item.currency
-        cur_dict['type'] = 'income'
-        date_time = item.date_time
-        income[date_time] = cur_dict
-    for item in outcome_transactions:
-        cur_dict ={}
-        cur_dict['senders_account'] = item.senders_account
-        cur_dict['receivers_account'] = item.receivers_account
-        cur_dict['amount'] = item.amount
-        cur_dict['currency'] = item.currency
-        cur_dict['type'] = 'outcome'
-        date_time = item.date_time
-        outcome[date_time] = cur_dict
+    income = utils.gather_transactions(income_transactions, 'income')
+    outcome = utils.gather_transactions(outcome_transactions, 'outcome')
+    print('income', income)
+    print('outcome', outcome)
     income.update(outcome)
     result['transactions'] = income
     return jsonify(result), status_code
